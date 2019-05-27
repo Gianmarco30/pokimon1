e12b317fc676be9e8f615fc5c49e977e5b224a2b
Gianmarco Loyola <gian711301@gmail.com>
2019-05-27 12:49:42 -0500

    historia 1

---
+++ pokimon/src/pokimon/Pokimon.java
@@ -0,0 +1,17 @@
+
+package pokimon;
+
+import entidades.JFventanaprincipal;
+
+
+public class Pokimon {
+
+   
+    public static void main(String[] args) {
+        JFventanaprincipal marco = new JFventanaprincipal();
+        marco.setVisible(true);
+        
+        
+    }
+    
+}
